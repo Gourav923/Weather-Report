f8de8829955cbe6579679938db08a77a2af0395f
Gourav Mehta <153151425+Gourav923@users.noreply.github.com>
2024-06-01 16:41:05 +0530

    Add files via upload

---
+++ WeatherReport.py
@@ -0,0 +1,23 @@
+import requests
+# json used here for covert text to dictionary
+import json
+#pyttsx3 is tused to text to speech
+import pyttsx3
+while True:
+    city=input('Enter name of the city\n') #take input from the user
+    if city=='exit':
+        break
+    #6b5807874f994c97881103137243105&q this is api key I used in it
+    url= f"https://api.weatherapi.com/v1/current.json?key=6b5807874f994c97881103137243105&q={city}"
+
+    r= requests.get(url)
+
+    #print(r.text) use this to get info in text and here all the info about weather in  in your preferred city
+    wdic= json.loads(r.text)
+    w= wdic['current']['temp_c']
+    print(w)
+    #this part is used to tell the temperature of the city that you entered
+    h= pyttsx3.init()    
+    h.say(f"The current weather in {city} is {w} degree celsius")
+    h.runAndWait()
+        
